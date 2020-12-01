0081dedc399e1091c47e05fcb91b7d6f229e8258
cheshrst <seva1999@yandex.ru>
2020-12-01 21:52:38 +0300

    1232132131123213uio

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,6 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ListView;
 public class Controller {
